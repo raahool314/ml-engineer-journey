e7827dbe5043f7e0c19450d9ba13b87de50a6815
Rahul Deshmukh <raahool314@users.noreply.github.com>
2026-07-05 18:22:55 +0530

    day1: identity card project

---
+++ day1_project.py
@@ -0,0 +1,16 @@
+name = "Rahul"
+age = "36"
+city = "Pune"
+role = "ML Engineer (in training)"
+fun_fact = "I like Coffee"
+experience = "0 years (Day 1 of 84)"
+print("=" * 32)
+print("        DEVELOPER ID CARD ")
+print("=" * 32)
+print(f"Name     : {name}")
+print(f"Age      : {age}")
+print(f"City     : {city}")
+print(f"Role     : {role}")
+print(f"Fun Fact : {fun_fact}")
+print(f"Experience: {experience}")
+print("=" * 32)
